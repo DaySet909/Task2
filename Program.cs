0065e2ebca8189ced2c5c70a3d95e08f359e00c0
DaySet909 <78254936+DaySet909@users.noreply.github.com>
2022-05-31 00:17:52 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,19 +10,18 @@ namespace Junior
     {
         static void Main(string[] args)
         {
-            byte a = 17;
-            sbyte b = 3;
-            short c = 4;
-            ushort d = 5;
-            int e = -6;
-            uint f = 7;
-            long g = -1000000000000000000;
-            ulong h = 9000000000000000000;
-            Console.WriteLine("Byte =" + Convert.ToString(a, 2) + ", SByte =" + Convert.ToString(b, 2));
-            Console.WriteLine("Short =" + c + ", UShort =" + d);
-            Console.WriteLine("Int =" + e + ", UInt =" + f);
-            Console.WriteLine("Long =" + g + ", ULong =" + h);
-
+            byte sign;
+            sbyte run;
+            short hits;
+            ushort car;
+            int health;
+            uint coin;
+            long millesAway;
+            ulong step;
+            bool variant;
+            string dialog;
+            float range;
+            double angle;
         }
     }
 }
